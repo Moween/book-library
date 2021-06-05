6203e0b781b4f077c0036cf0605aae51e7da1815
Moween <moween1998@gmail.com>
2021-06-05 12:11:03 +0100

    Refactor code - Refactor Book and BookCover class. - Change variable names - Change the submit event function name.

--- src/script.js
+++ src/script.js
@@ -3,7 +3,11 @@ const submitBtn = document.querySelector('#form');
 const bookTitleInput = document.querySelector('#book-title');
 const bookAuthorInput = document.querySelector('#book-author');
 const numOfPagesInput = document.querySelector('#num-of-pages');
+const readInput = document.querySelector('#read');
 const booksContainer = document.querySelector('#books-container');
+let clicked = false;  
+  
+
 
 let bookArrayCopy = localStorage.getItem('books');
 // A copy of local Storage data
@@ -17,12 +21,10 @@ if (bookArrayCopy) {
 
 newBookBtn.addEventListener('click', (e) => {
   e.preventDefault();
+  console.log(e.target)
   const formContainer = document.querySelector('.form-container');
-  if (formContainer.style.display === 'none') {
-    formContainer.style.display = 'block';
-  } else {
-    formContainer.style.display = 'none';
-  }
+  formContainer.style.display = 
+    (formContainer.style.display === 'block' ? 'none': 'block');  
 });
 
 const clearInput = (e) => {
@@ -31,29 +33,34 @@ const clearInput = (e) => {
 };
 
 class Book {
-  constructor() {
-    this.title = document.querySelector('#book-title').value;
-    this.author = document.querySelector('#book-author').value;
-    this.numberOfPages = document.querySelector('#num-of-pages').value;
-    this.id = Date.now();
+  constructor(book) {
+    this.title = book.title;
+    this.author = book.author;
+    this.numberOfPages = book.numberOfPages;
+    this.id = book.id;
+    this.readStatus = book.readStatus;
   }
 }
 
-const handleAddBook = (e) => {
+const handleSubmit = (e) => {
   e.preventDefault();
   clearInput(e);
-  const book = new Book();
-  const {
-    title, author, numberOfPages, id,
-  } = book;
+  const newBook = {};
+  newBook.title = e.target['book-title'].value;
+  newBook.author = e.target['book-author'].value;
+  newBook.numberOfPages = e.target['num-of-pages'].value;
+  newBook.id = Date.now().toString();
+  newBook.readStatus = e.target['read'].value;
+
+  const book = new Book(newBook);
 
   let booksArray = localStorage.getItem('books');
-  if (booksArray) {
-    booksArray = JSON.parse(booksArray);
-  }
+  booksArray = JSON.parse(booksArray);
+  
   booksArray.push({
-    title, author, numberOfPages, id,
-  });
+    title, author, numberOfPages, id, readStatus
+  } = book);
+
   bookArrayCopy = [...booksArray];
   // Set Local Storage
   localStorage.setItem('books', JSON.stringify(booksArray));
@@ -63,26 +70,74 @@ const handleAddBook = (e) => {
 class BookCover {
   constructor(book) {
     this.card = document.createElement('div');
-    this.className = 'book-card';
-    const h04 = `${book.title}`;
+    this.card.className = 'book-card';
+    const h04 = document.createElement('h4'); 
+    h04.innerHTML = `Title: <cite> ${book.title}</cite>`
+    h04.className = 'book-title';
     const pElem = document.createElement('p');
-    pElem.textContent = `${book.author}`;
+    pElem.textContent = `Author: ${book.author}`;
+    pElem.className = 'book-author';
     const pElem2 = document.createElement('p');
-    pElem2.textContent = `${book.numberOfPages}`;
-    this.card.append(h04, pElem, pElem2);
-    this.deleteBtn = document.createElement('button');
-    this.deleteBtn.className = 'btn btn-xs btn-danger delete';
-    this.deleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
-    this.deleteBtn.onclick = handleDeleteBooks;
-    this.readBtn = document.createElement('button');
-    this.readBtn.className = 'btn btn-xs btn-success read-btn';
-    this.readBtn.textContent = 'Read';
-    this.readBtn.onclick = handleReadBooks;
+    pElem2.textContent = `${book.numberOfPages} pages`;
+    const deleteBtn = document.createElement('button');
+    deleteBtn.className = 'btn btn-sm btn-danger delete-btn';
+    deleteBtn.innerHTML = '<i class="fas fa-trash"></i>';
+    deleteBtn.onclick = this.handleDeleteBooks;
+    deleteBtn.setAttribute('data-id', book.id);
+    const readBtn = document.createElement('button');
+    readBtn.setAttribute('data-id', book.id);
+    readBtn.className = 'btn btn-sm btn-success read-btn';
+    readBtn.textContent = 'Read';
+    readBtn.onclick = this.toggleRead;
+    this.card.append(
+      h04, pElem, pElem2, readBtn, deleteBtn
+    );
+    this.handleDeleteBooks = this.handleDeleteBooks.bind(this);
+    this.toggleRead = this.toggleRead.bind(this);
+  }
+
+  handleDeleteBooks = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const { currentTarget: { dataset: { id: deleteBook } } } = e;
+  
+    let booksArray = localStorage.getItem('books');
+    booksArray = JSON.parse(booksArray);
+    // Deletes a book from the localStorage
+    booksArray = booksArray.filter((book) => book.id !== deleteBook);
+    // Spread new books Array after delete to the booksArrayCopy
+    bookArrayCopy = [...booksArray];
+    // Resets local storage
+    localStorage.setItem('books', JSON.stringify(booksArray));
+    // Update DOM
+    displayBooks();
+  };
+  
+  toggleRead = (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const {target: {dataset: {id: book_id}}} = e;
+  
+    let booksArray =  localStorage.getItem('books');
+    booksArray = JSON.parse(booksArray);
+  
+    // Adss a read key to the localStorage Data
+    booksArray = booksArray.map(book => {
+      if(book.id === book_id) {
+        book.readStatus = (book.readStatus === true ? false : true);
+      }
+      return book;
+    })
+    // Spread new books Array after to the booksArrayCopy
+    bookArrayCopy = [...booksArray];
+    // Resets local storage
+    localStorage.setItem('books', JSON.stringify(booksArray));
+    displayBooks();
   }
 }
 
 class LibraryLog {
-  constructor(book) {
+  constructor() {
     this.table = document.createElement('table');
     this.table.className = 'table';
     const tableCaption = document.createElement('caption');
@@ -98,11 +153,11 @@ class LibraryLog {
           break;
         case 1:
           tableHeading.textContent = 'Read';
-          tableData.textContent = `${bookArrayCopy.filter((book) => book.read).length}`;
+          tableData.textContent = `${bookArrayCopy.filter(book => book.readStatus === String(true)).length}`;
           break;
         case 2:
           tableHeading.textContent = 'Not Read';
-          tableData.textContent = `${bookArrayCopy.filter((book) => !(book.read)).length}`;
+          tableData.textContent = `${bookArrayCopy.filter(book => book.readStatus === String(false)).length}`;
           break;
         default:
           tableHeading.textContent = 'Nothing';
@@ -115,58 +170,25 @@ class LibraryLog {
 
 const displayBooks = () => {
   booksContainer.innerHTML = '';
-  bookArrayCopy.forEach((book) => {
-    const mybook = new BookCover(book);
-    const { card, deleteBtn, readBtn } = mybook;
-    deleteBtn.setAttribute('data-id', book.id);
-    readBtn.setAttribute('data-id', book.id);
-    card.append(readBtn, deleteBtn);
+  bookArrayCopy.forEach(newBook => {
+    const mybook = new BookCover(newBook);
+    const { card } = mybook;
     booksContainer.append(card);
   });
-  const lib = new LibraryLog();
-  booksContainer.append(lib.table);
+  if(!bookArrayCopy.length) {
+    document.querySelector('.table-container').style.display = 'none';
+  }else {
+    const lib = new LibraryLog();
+    const { table } = lib;
+    document.querySelector('.table-container').style.display = 'block';
+    document.querySelector('.table-container').innerHTML = '';
+    document.querySelector('.table-container').append(table);
+  }
 };
-
-const handleDeleteBooks = (e) => {
-  e.preventDefault();
-  e.stopPropagation();
-  const { currentTarget: { dataset: { id: deleteBook } } } = e;
-
-  let booksArray = localStorage.getItem('books');
-  booksArray = JSON.parse(booksArray);
-  // Deletes a book from the localStorage
-  booksArray = booksArray.filter((book) => book.id !== Number(deleteBook));
-  // Spread new books Array after delete to the booksArrayCopy
-  bookArrayCopy = [...booksArray];
-  // Resets local storage
-  localStorage.setItem('books', JSON.stringify(booksArray));
-  // Delete From DOM
-  displayBooks();
-};
-
-const handleReadBooks = (e) => {
-  e.preventDefault();
-  e.stopPropagation();
-  const {target: {dataset: {id: readBook}}} = e;
-
-  let booksArray =  localStorage.getItem('books');
-  booksArray = JSON.parse(booksArray);
-
-  // Adss a read key to the localStorage Data
-  booksArray.forEach(book => {
-    if(book.id === Number(readBook)) {
-      book.read = true;
-    }
-  })
-  // Spread new books Array after to the booksArrayCopy
-  bookArrayCopy = [...booksArray];
-  // Resets local storage
-  localStorage.setItem('books', JSON.stringify(booksArray));
-  displayBooks();
-}
 
 displayBooks();
-submitBtn.addEventListener('submit', handleAddBook);
+submitBtn.addEventListener('submit', handleSubmit);
 bookTitleInput.addEventListener('click', clearInput);
 bookAuthorInput.addEventListener('click', clearInput);
+readInput.addEventListener('click', clearInput);
 numOfPagesInput.addEventListener('click', clearInput);
